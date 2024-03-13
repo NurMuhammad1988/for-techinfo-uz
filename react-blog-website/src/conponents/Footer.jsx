4cfd768366b0c94654479903c9be7bcf1ac5924a
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-03-14 04:18:46 +0500

    flu very very srong flu qh qh qh

--- react-blog-website/src/conponents/Footer.jsx
+++ react-blog-website/src/conponents/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { FaFacebook, FaInstagram } from "react-icons/fa6";
+import { BsTwitterX } from "react-icons/bs";
 
 const Footer = () => {
     return (
         <div className="bg-gray-900">
             <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4">
-
                 <div className="grid mb-8 lg:grid-cols-6">
                     <div className="grid grid-cols-2 gap-5 lg:col-span-4 md:grid-cols-4">
                         {/* category 1*/}
@@ -186,7 +187,7 @@ const Footer = () => {
                                         className="text-gray-500 transition-colors duration-300 hover:text-orange-500"
                                         href="/"
                                     >
-                                        Wiki
+                                        Wikings
                                     </a>
                                 </li>
 
@@ -209,15 +210,38 @@ const Footer = () => {
                                 </li>
                             </ul>
                         </div>
-
                     </div>
 
-                           {/* subscription*/}
-                           <div className="md:max-w-md lg:col-span-2 lg:mt-0 mt-5">
-                            <p className="font-medium tracking-wide text-gray-300">
-                               Subscribe for updates
-                            </p>
-                            {/* <ul className="mt-2 space-y-2">
+                    {/* subscription*/}
+                    <div className="md:max-w-md lg:col-span-2 lg:mt-0 mt-5">
+                        <p className="font-medium tracking-wide text-gray-300">
+                            Subscribe for updates
+                        </p>
+
+                        <form className="mt-4 flex flex-col md:flex-row">
+                            <input
+                                className="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm aspect-auto md:mr-2 md:mb-0 focus:bg-purple-400 focus:outline-none"
+                                type="email"
+                                name="email"
+                                id="email"
+                            />
+                            <button
+                                className="inline-flex items-center  justify-center h-12 px-6  font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:bg-orange-500 focus:outline-none border"
+                                type="submit"
+                            >
+                                Subscribe
+                            </button>
+                        </form>
+
+                        <p className="mt-4 text-sm text-gray-500 ">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing
+                            elit. Assumenda deleniti magni molestiae. Deleniti
+                            voluptatum hic nihil esse id voluptate voluptates
+                            natus? Libero voluptate accusamus nisi sit
+                            praesentium!
+                        </p>
+
+                        {/* <ul className="mt-2 space-y-2">
                                 <li>
                                     <a
                                         className="text-gray-500 transition-colors duration-300 hover:text-orange-500"
@@ -263,13 +287,36 @@ const Footer = () => {
                                     </a>
                                 </li>
                             </ul> */}
+                    </div>
+                </div>
 
+                <div className="flex flex-col justify-between pt-8 pb-10 border-t border-gray-800 sm:flex-row ">
+                    <p className="text-sm text-gray-500">
+                        © Copyriht 2024 | All right resereved.
+                    </p>
 
+                    <div className="flex items-center mt-4 space-x-4 sm:mt-0    ">
+                        <a
+                            className="text-gray-500 transition-all duration-300 hover:text-orange-500 "
+                            href=""
+                        >
+                            <BsTwitterX className="h-6 w-6" />
+                        </a>
 
+                        <a
+                            className="text-gray-500 transition-all duration-300 hover:text-orange-500 "
+                            href=""
+                        >
+                            <FaInstagram className="h-6 w-6" />
+                        </a>
 
-
-                            {/* 3:07:24 chi minutda qoldi */}
-                        </div>
+                        <a
+                            className="text-gray-500 transition-all duration-300 hover:text-orange-500 "
+                            href=""
+                        >
+                            <FaFacebook className="h-6 w-6" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>

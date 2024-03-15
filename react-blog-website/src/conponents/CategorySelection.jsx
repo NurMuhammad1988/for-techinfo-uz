fbf5ff8f63914fe426fc90decc98c36d37acfb96
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-03-15 08:27:32 +0500

    finished

--- react-blog-website/src/conponents/CategorySelection.jsx
+++ react-blog-website/src/conponents/CategorySelection.jsx
@@ -5,7 +5,12 @@ const CategorySelection = ({ onSelectCategory, activeCategory }) => {
 
     return (
         <div className="px-4 mb-8 lg:space-x-16 flex flex-wrap items-center border-b-2 py-5 text-gray-900 font-semibold">
-            <button className={`lg:ml-12 ${activeCategory ? "" : "active-button"}`} onClick={() => onSelectCategory(null)}>ALL</button>
+            <button
+                className={`lg:ml-12 ${activeCategory ? "" : "active-button"}`}
+                onClick={() => onSelectCategory(null)}
+            >
+                ALL
+            </button>
             {/* yani agar all buttoni activecategory bo'lsa class bo'masin agar activegategory bo'lsa active-button clasi qo'shilsin */}
             {categories.map((category) => (
                 <button
@@ -21,6 +26,5 @@ const CategorySelection = ({ onSelectCategory, activeCategory }) => {
         </div>
     );
 };
-
 
 export default CategorySelection;
